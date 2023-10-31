813637c492bd12863623f042f7041ef859a69bfa
Jeremiah Deffendall <jddeffendal2@bsu.edu>
2023-10-31 11:58:37 -0400

    Initial Vue Project is Created

---
+++ portofilo-page/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <MyPortfolio />
+</template>
+
+<script setup>
+import MyPortfolio from './components/MyPortfolio.vue'
+</script>
+
+<style scoped>
+/* header {
+  line-height: 1.5;
+} */
+
+/* @media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+} */
+</style>
